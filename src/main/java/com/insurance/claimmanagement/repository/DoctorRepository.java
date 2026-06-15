fe509743ea3352a2dd6da3f6727f21467a3e1c21
GowthamB04 <GowthamBalaraman2004@gmail.com>
2026-06-15 14:39:05 +0530

    done

--- src/main/java/com/insurance/claimmanagement/repository/DoctorRepository.java
+++ src/main/java/com/insurance/claimmanagement/repository/DoctorRepository.java
@@ -3,7 +3,6 @@ package com.insurance.claimmanagement.repository;
 import com.insurance.claimmanagement.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
